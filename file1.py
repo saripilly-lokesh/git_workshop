00ca76e8d4309bb460eda258c6840d164e520239
saripilly-lokesh <lokeshsaripilly@gmail.com>
2025-04-03 12:07:05 +0530

    cconflict

--- file1.py
+++ file1.py
@@ -2,3 +2,24 @@ print("Hi")
 print("lokesh")
 print("CBIT")
 print("CSE")
+def calculator():
+    num1 = float(input("Enter first number: "))
+    op = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+    if op == "+":
+        print('addition operation')
+        print(num1+num2)
+        print('Result(num1+mun2)')
+    elif op == "-":
+       print('addition operation')
+       print(num1+num2)
+       print('Result(num1+mun2)')
+    elif op == "*":
+         print('addition operation')
+         print(num1+num2)
+         print('Result(num1+mun2)')
+    elif op == "/":
+        return "Error! Division by zero." if num2 == 0 else num1 / num2
+    else:
+        return "Invalid operator!"
+print(calculator())
